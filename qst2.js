d6e51a6e58e13cf88aca58621ef450204805eb31
judahluke <judahluke58@gmail.com>
2024-04-01 15:56:07 +0300

    initial commit

---
+++ qst2.js
@@ -0,0 +1,18 @@
+function generateArray(start, end) {
+    const result = [];
+  // allows the iniitial value input to add a value greater than one until it reaches the last value
+    if (start <= end) {
+      for (let p = start; p <= end; p++) {
+        result.push(p);
+      }
+      
+    }
+    return result;
+  }
+  
+
+  console.log(generateArray('start', 'end'));
+
+  // case exapmle
+  console.log(generateArray(-4, 7)); 
+  console.log(generateArray(4,7))
